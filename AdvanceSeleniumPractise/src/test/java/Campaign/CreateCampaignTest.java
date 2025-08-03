6c04aafe5e793919bb40c95d75224d89b830c46b
KuNaL-S-PaTiL <kunal.sarjerao.patil@gmail.com>
2025-08-03 13:30:37 +0530

    Update CreateCampaignTest.java

--- AdvanceSeleniumPractise/src/test/java/Campaign/CreateCampaignTest.java
+++ AdvanceSeleniumPractise/src/test/java/Campaign/CreateCampaignTest.java
@@ -42,6 +42,7 @@ import POM_Repo.ValidationCampaignPage;
 
 //@Listeners(Generic_Utilities.ExtentReportImp.class)
 
+// I am Pulling pull1
 public class CreateCampaignTest extends Base_Class {
 	@Test(groups="SmokeTest")
 public void createCampaignTest() throws Throwable {
