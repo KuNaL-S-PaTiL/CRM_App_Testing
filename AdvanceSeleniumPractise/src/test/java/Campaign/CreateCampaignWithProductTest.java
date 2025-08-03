2909820ab948816ac0dac17890dcdd5523f14b74
Kunal <Kunal@LAPTOP-9VAIVGVE>
2025-08-03 18:57:23 +0530

    branch1

--- AdvanceSeleniumPractise/src/test/java/Campaign/CreateCampaignWithProductTest.java
+++ AdvanceSeleniumPractise/src/test/java/Campaign/CreateCampaignWithProductTest.java
@@ -48,6 +48,7 @@ import POM_Repo.WindowSwitchingPage;
 //->Logout from the application. 
 
 
+// branch1 --> smoke
 public class CreateCampaignWithProductTest extends Base_Class{
 	@Test(groups="SmokeTest")
 public void createCampaignTest() throws Throwable {
