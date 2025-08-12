a48195347cdd7c0ccf34ecf063e5bf6ecc3ecd32
KuNaL-S-PaTiL <kunal.sarjerao.patil>
2025-08-12 19:51:54 +0530

    poll scm

--- AdvanceSeleniumPractise/src/test/java/Campaign/CreateCampaignTest.java
+++ AdvanceSeleniumPractise/src/test/java/Campaign/CreateCampaignTest.java
@@ -29,6 +29,8 @@ import POM_Repo.Home_Page;
 import POM_Repo.Login_Page;
 import POM_Repo.ValidationCampaignPage;
 
+//modified for Poll SCM
+
 //Login to vtiger application
 //->mouseOverOn more Link
 //->click on campaigns
